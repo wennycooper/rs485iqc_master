c21d1d6d584b9797c32056ec174033675964ed1c
wennycooper <kevin.kuei.0321@gmail.com>
2016-07-19 15:20:30 +0800

    ...

--- rs485iqc_master.ino
+++ rs485iqc_master.ino
@@ -2,6 +2,7 @@
 #define RS485Transmit    HIGH
 #define RS485Receive     LOW 
 #define SLAVE   1
+#define LOOPTIME 500
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,25 +11,34 @@ void setup() {
   digitalWrite(8, RS485Receive); //DE,RE=LOW, RX enabled
 
   Serial.begin(115200);     // Console 
-  Serial1.begin (1000000);  // TO MAX485
+  Serial1.begin (115200);  // TO MAX485
 }
 
 int recv;
 int i;
+unsigned long lastMilli = 0;                    // loop timing 
+long dT = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
   
-
-  for (i=0; i<100; i++) {
-    Serial.print("Send: ");
+  if((millis()-lastMilli) >= LOOPTIME)  {                                    // enter tmed loop
+    dT = millis()-lastMilli;
+    lastMilli = millis();
+    
+    Serial.print("Sent: ");
     Serial.println(i, HEX);
     
     sendCmd(i, SLAVE);
     recv = read_slave(SLAVE);
-    delay(1000);    
+    delay(1);    
 
     Serial.print("Got: ");
     Serial.println(recv, HEX);
+
+    i++;
+    if (i>100) i = 0;
+    
   }
 
 
@@ -51,18 +61,31 @@ int read_slave(int slave)
   {
     char rT = (char)Serial1.read(); //read target speed from mega
           if(rT == '{') //start byte
-            {
+          {
               char commandArray[3];
               Serial1.readBytes(commandArray,3);
               byte rA=commandArray[0]; //device address
               byte rH=commandArray[1]; //high byte
               char rP=commandArray[2]; //stop byte
+
+              Serial.print(rT, HEX);
+              Serial.print(rA, HEX);
+              Serial.print(rH, HEX);
+              Serial.print(rP, HEX);
+              
               if(rP=='}' && rA==slave)         
-                {                 
+              {             
+                  
                   return rH;
-                }
-            }
-  }   
+              } else {
+                return 89;
+
+              }
+          }
+  } else {
+    return 88;
+  }
+    
   
 }
 
@@ -79,7 +102,7 @@ void sendCmd(int cmd,int slave)
   Serial1.write(sA); 
   Serial1.write(sH); 
   Serial1.write(sP);
-  delayMicroseconds(10);
+  delayMicroseconds(300);
   digitalWrite(8, RS485Receive); //DE,RE=LOW, RX enabled
   delay(1);  // delay 1ms
 }
